41ba259a660b8ae350075563f798c994957d6f79
Jeffrey Lee Ye <yej9719@yahoo.com>
2017-10-21 18:27:53 -0700

    Another start

---
+++ Assets/_OurScripts/DrawLinesFromPoints.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLinesFromPoints : MonoBehaviour {
+
+    LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+        GetDataPoints.DataPointsChanged += updateLineRenderer;
+        StartCoroutine(startAddInitialValues());
+	}
+
+    private IEnumerator startAddInitialValues() {
+        yield return StartCoroutine(addInitialValues());
+        //StartCoroutine(test());
+    }
+
+    IEnumerator addInitialValues() {
+        while (true) {
+            yield return new WaitForSeconds(.1f);
+            print("still checking");
+            //if (GetDataPoints.getDataPoints() != null) {
+                for (int i = 0; i < lineRenderer.positionCount; ++i) {
+                    GetDataPoints.getDataPoints().Add(lineRenderer.GetPosition(i));
+                }
+                yield break;
+            //}
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    IEnumerator test() {
+         while (true) {
+            yield return new WaitForSeconds(3f);
+            print("Adding...");
+            GetDataPoints.addPoint(GetDataPoints.getDataPoints()[GetDataPoints.getDataPoints().Count - 1] - Vector3.left + Random.insideUnitSphere * 1.5f);
+        }
+    }
+
+    private void updateLineRenderer() {
+        List<Vector3> dataPoints = GetDataPoints.getDataPoints();
+        print(dataPoints.Count);
+        lineRenderer.positionCount = dataPoints.Count;
+        for (int i = 0; i < dataPoints.Count; ++i) {
+            lineRenderer.SetPosition(i, dataPoints[i]);
+        }
+        print("Updating Line Renderer");
+    }
+}
---
+++ Assets/_OurScripts/GetDataPoints.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetDataPoints : MonoBehaviour {
+
+    static private List<Vector3> dataPoints = new List<Vector3>();
+    static public List<Vector3> getDataPoints() {
+        return dataPoints;
+    }
+    static private float closenessThreshold = .1f;
+
+    public delegate void dataPointsChanged();
+    public static event dataPointsChanged DataPointsChanged;
+
+	// Use this for initialization
+	void Awake() {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void addPoint(Vector3 pointToAdd, float? newThreshold = null) {
+        if (newThreshold.HasValue) {
+            closenessThreshold = newThreshold.Value;
+        }
+        if (dataPoints.Count == 0 || Vector3.Distance(pointToAdd, dataPoints[dataPoints.Count-1]) > closenessThreshold) {
+            dataPoints.Add(pointToAdd);
+            DataPointsChanged();
+        }
+    }
+
+}
